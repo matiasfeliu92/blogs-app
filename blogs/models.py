49db4ca93862fa50fa6be1d8dc1b1b9567fe02a5
matiasfeliu92 <matumazparrote@gmail.com>
2023-06-10 22:07:01 -0300

    refactor: add db models for Category and Posts

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True)
