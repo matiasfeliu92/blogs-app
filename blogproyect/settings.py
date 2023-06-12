1b90a926f458661b9d7d5fd8fab4e486541dc487
matiasfeliu92 <matumazparrote@gmail.com>
2023-06-12 14:32:36 -0300

    refactor: update Alloweb hosts in settings.py

--- blogproyect/settings.py
+++ blogproyect/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'https://blogs-app-w9e1.onrender.com/']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:    
