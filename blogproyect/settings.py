30bfff5c106fc003267c250ceeaebf5ceed3502d
matiasfeliu92 <matumazparrote@gmail.com>
2023-06-12 12:24:21 -0300

    refactor: update .gitignore and databases in settings.py

--- blogproyect/settings.py
+++ blogproyect/settings.py
@@ -86,6 +86,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'production_db': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': 5432,
     }
 }
 
