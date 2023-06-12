d9a32e76f7901c2327c48b8ed4a82507095d5363
matiasfeliu92 <matumazparrote@gmail.com>
2023-06-12 11:37:39 -0300

    refactor: update admin.py file, for modify the admin panel to access to app data

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Category, Post
 # Register your models here.
+
+class PostsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'category')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+admin.site.register(Post, PostsAdmin)
+admin.site.register(Category, CategoryAdmin)
