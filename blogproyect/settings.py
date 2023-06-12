531160608cf5f394014ce8888e9c946883a4c15f
matiasfeliu92 <matumazparrote@gmail.com>
2023-06-12 12:43:41 -0300

    refactor: update database config in setting.py

--- blogproyect/settings.py
+++ blogproyect/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'blogproyect.wsgi.application'
 
 if 'PRODUCTION' in os.environ:
     DATABASES = {
-        'default': {
+        'production': {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.environ.get('DB_NAME'),
             'USER': os.environ.get('DB_USER'),
