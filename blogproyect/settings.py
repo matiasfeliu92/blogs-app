04a8343d5735d08fcbf6e92b1c031c4b3092cbec
matiasfeliu92 <matumazparrote@gmail.com>
2023-06-12 11:47:10 -0300

    refactor: update settings.py, add build.sh file for deploy and update requirements.txt file

--- blogproyect/settings.py
+++ blogproyect/settings.py
@@ -35,6 +35,7 @@ if RENDER_EXTERNAL_HOSTNAME:
 # Application definition
 
 INSTALLED_APPS = [
+    'render.apps.RenderConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
