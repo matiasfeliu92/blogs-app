967fc3b115290ac9c5a7ef3af9566f457c0fff8c
matiasfeliu92 <matumazparrote@gmail.com>
2023-06-12 16:56:51 -0300

    refactor: delete PostView clas from models

--- blogs/models.py
+++ blogs/models.py
@@ -9,38 +9,3 @@ class Post(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     pub_date = models.DateTimeField(auto_now_add=True)
     image = models.CharField(max_length=300, null=True)
-
-class PostView(models.Model):
-    id = models.IntegerField(primary_key=True)
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    category_name = models.CharField(max_length=200)
-    pub_date = models.DateTimeField()
-    image = models.CharField(max_length=300, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'post_view'  # Set the name of the PostgreSQL view
-
-def generate_post_view():
-    with connection.cursor() as cursor:
-            
-        cursor.execute("DROP VIEW post_view;")
-        
-        # Create the view
-        cursor.execute("""
-            CREATE VIEW post_view AS
-            SELECT
-                post.id,
-                post.title,
-                post.content,
-                category.name AS category_name,
-                post.pub_date,
-                post.image
-            FROM
-                blogs_post AS post
-            INNER JOIN
-                blogs_category AS category ON post.category_id = category.id
-        """)
-
-generate_post_view()
