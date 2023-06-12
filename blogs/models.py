ec696c82af322919cfdbac087c166f56b1aecd7b
matiasfeliu92 <matumazparrote@gmail.com>
2023-06-12 11:38:11 -0300

    refactor: update Category and Post database models

--- blogs/models.py
+++ blogs/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, unique=True)
     content = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     pub_date = models.DateTimeField(auto_now_add=True)
+    image = models.CharField(max_length=300, null=True) 
