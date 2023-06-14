9cfc4b23990b9d73927de66473d3d7c901948909
matiasfeliu92 <matumazparrote@gmail.com>
2023-06-14 09:46:57 -0300

    refactor: update functin used in / route path

--- blogs/urls.py
+++ blogs/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('api', views.index, name='index'),
-    path('', views.all_posts, name='get_posts'),
+    path('', views.filter_posts, name='get_posts'),
     path('new_post', views.new_post, name='create_post'),
 ]
